76ad9269e270acd70761b1cf15fbce3a80102878
gabribp <gblancop06@iescastelar.com>
2024-02-06 14:41:23 +0100

    agregado ejemplo de javascript

---
+++ ejemplo.js
@@ -0,0 +1,21 @@
+/**
+ * Prueba de API
+ * @module API
+ * @author Luis Ferreira <mi.email@iescastelar.com>
+ */
+ 
+/** API Class description */
+class API {
+    /**
+     * Constructor de clase
+     * @param {Objeto} config ejemplo de descripción
+     */
+     constructor( config ){
+         /** @private */
+         this.uno = config;
+         /** @public */
+         this.dos = "ejemplo";
+     }
+}
+module.exports = MiClase;
+// prueba de cambios
